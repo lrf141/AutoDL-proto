0a83dd0402ae620075f31289974e48b344631f19
lrf141 <ghost141.kentyo@gmail.com>
2019-02-12 01:33:29 +0900

    websocket周辺を修正

--- resources/views/submit.blade.php
+++ resources/views/submit.blade.php
@@ -3,15 +3,6 @@
 
 
 @section('content')
-    <!-- ここあとでcdnからyarnに変更する -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.js"></script>
-    <script type="text/javascript">
-        const socket = io();
-        const chName = document.getElementById('xid').dataset.xid;
-        socket.on(chName, (msg) => {
-            document.getElementById('process-box').insertAdjacentElement('afterbegin', "<p>"+msg+"</p>");
-        });
-    </script>
     <div id="xid" data-xid="{{$xid}}"></div>
     <div class="container">
         <div class="row justify-content-center">
@@ -24,4 +15,20 @@
             </div>
         </div>
     </div>
+    <!-- ここあとでcdnからyarnに変更する -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.js"></script>
+    <script type="text/javascript">
+        //ws -> wss
+        //using local setting
+        const socket = io('ws://172.30.0.1:8000');
+        const chName = document.getElementById('xid').dataset.xid;
+        //console.log(socket);
+        console.log(chName);
+        socket.emit('register', {xid: chName});
+        socket.on('process-message', (msg) => {
+            console.log(msg);
+            document.getElementById('process-box').innerHTML += msg.body;
+            //document.getElementById('process-box').insertAdjacentElement('afterbegin', "<p>"+msg+"</p>");
+        });
+    </script>
 @endsection
