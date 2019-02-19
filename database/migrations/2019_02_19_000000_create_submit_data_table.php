00e1a419194e4e63e00262e651e2d846ffe5d13b
lrf141 <ghost141.kentyo@gmail.com>
2019-02-20 02:57:50 +0900

    マイグレーションを追加

---
+++ database/migrations/2019_02_19_000000_create_submit_data_table.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lrf141
+ * Date: 19/02/19
+ * Time: 23:29
+ */
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubmitDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('submit', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('userid')->nullable(false);
+            $table->string('xid')->nulllable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('submit');
+    }
+}
